d2a1cb05972c0176a755142d2af70a7fe3500b02
Dxdii <lukas.dietinger@gmail.com>
2022-02-15 10:56:36 +0100

    first try to make Linearlist

---
+++ src/Linearlist.java
@@ -0,0 +1,28 @@
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+public class Linearlist{
+  int value;
+  int Element;
+  static int count = 0;
+
+    public Linearlist(int i){
+        Element = i;
+        count++;
+    }
+    public void add(int i){
+    value = i;
+    }
+    public void Printall(){
+
+    }
+    public void get(int i)
+    {
+
+        System.out.println();
+
+    }
+
+}
