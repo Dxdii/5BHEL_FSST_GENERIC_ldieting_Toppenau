35b9b403a04e57f9354623bcfd7d87e8d8912847
Dxdii <lukas.dietinger@gmail.com>
2022-02-15 12:20:00 +0100

    Linearlist ausgabe funktioniert

--- src/Linearlist.java
+++ src/Linearlist.java
@@ -1,23 +1,39 @@
-public class Linearlist{
+public class Linearlist {
 
-  int plast;
-  int pnew;
+    int value;
+    Linearlist pnew;
+    int count;
 
-
-
-    public Linearlist(int i){
-       pnew = i;
-    }
-    public void add(Linearlist a){
-      plast = a.plast;
-    }
-    public void Printall(){
-      System.out.println(pnew);
-
-    }
-    public void get(int i)
-    {
+    public Linearlist(int i) {
+        value = i;
 
     }
 
+    public void add(int i) {
+        if (pnew == null) {
+            pnew = new Linearlist(i);
+            count++;
+        } else {
+            pnew.add(i);
+
+        }
+    }
+
+    public void Printall() {
+        System.out.println(value);
+        if (pnew != null) {
+            pnew.Printall();
+        }
+
+
+    }
+
+    public void get(int i) {
+        if (count == i) {
+            System.out.println(value);
+
+        } else {
+            pnew.get(i);
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-
+    Linearlist root = new Linearlist(1);
+    root.add(3);
+    root.add(4);
+    root.add(5);
+    root.Printall();
+    root.get(3);
     }
 }
