efa6cc7eb07aa90e5509f6760bbfbc6bc92259dc
Dxdii <lukas.dietinger@gmail.com>
2022-02-15 11:20:56 +0100

    Linearlist Dietinger Main Oppenauer

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+    Linearlist root = new Linearlist(1);
+    root.add(new Linearlist(3));
+    root.add(new Linearlist(4));
 
+    root.Printall();
     }
 }
