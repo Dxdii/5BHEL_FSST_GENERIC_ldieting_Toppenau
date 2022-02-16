8293227719c9d97f5bc477d0ec5f95427cbf9192
Dxdii <lukas.dietinger@gmail.com>
2022-02-16 11:40:13 +0100

    New Main functiony by Oppenauer

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.function.Consumer;
 
 public class Main {
     public static void main(String[] args) {
+        Linearlist<Integer> root1 = new Linearlist<>(1);
+        root1.add(2);
+        root1.add(3);
+        root1.get(2);
+        root1.Printall();
+
         Linearlist<Schueler> root = new Linearlist<>(new Schueler("Brenda"));
         root.add(new Schueler("Dietinger"));
         root.add(new Schueler("Oppenauer"));
@@ -12,7 +20,8 @@ public class Main {
         root.get(1);
         System.out.println(root.count());
 
-        Iterator<Schueler> iterator = root.iterator();
+
+
 
         for (Schueler s:root)
         {
